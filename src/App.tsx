060a089d138f79f0a6ba3d3cb751d5964c88c5e4
saicharanmadhu08 <saicharanmadhu@gmail.com>
2021-01-27 21:25:40 -0600

    basic setup and adds react material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "pages/home";
 
 function App() {
   return (
-    <div className="App">
-      Start here
+    <div>
+      <Home />
     </div>
   );
 }
